2687e800aafaa191db67dd95c735bc35b2dcf3aa
kiki1027 <cdmp@yonyou.com>
2017-09-11 16:18:22 +0800

    枚举类

--- src/main/java/com/krt/ruanjian/course/enums/MajorEnum.java
+++ src/main/java/com/krt/ruanjian/course/enums/MajorEnum.java
@@ -6,19 +6,17 @@ package com.krt.ruanjian.course.enums;
  * @date 2017-9-9
  */
 public class MajorEnum {
-    private static final String CODE_TONGXIN = "1001";
-    private static final String CODE_COMPUTER_SCIENCE = "1002";
-    private static final String CODE_SOFT_PROJECT = "1003";
-    private static final String CODE_IOT = "1004";
-    private static final String CODE_NETWORK_PROJECT = "1005";
-    private static final String CODE_DIGITAL_META = "1006";
+    private static final String CODE_SOFT_PROJECT = "080902";
+    private static final String CODE_NETWORK_PROJECT = "080903";
+    private static final String CODE_INFO_SECURITY = "080904";
+    private static final String CODE_IOT = "080905";
+    private static final String CODE_DIGITAL_META_PROJECT = "080906";
 
-    private static final MajorEnum TONGXIN = new MajorEnum(CODE_TONGXIN, "通信工程");
-    private static final MajorEnum COMPUTER_SCIENCE = new MajorEnum(CODE_TONGXIN, "计算机科学与技术");
-    private static final MajorEnum SOFT_PROJECT = new MajorEnum(CODE_TONGXIN, "软件工程专业");
-    private static final MajorEnum IOT = new MajorEnum(CODE_TONGXIN, "物联网工程");
-    private static final MajorEnum NETWORK_PROJECT = new MajorEnum(CODE_TONGXIN, "网络工程专业");
-    private static final MajorEnum DIGITAL_META = new MajorEnum(CODE_TONGXIN, "数字媒体专业");
+    private static final MajorEnum SOFT_PROJECT = new MajorEnum(CODE_SOFT_PROJECT, "软件工程");
+    private static final MajorEnum NETWORK_PROJECT = new MajorEnum(CODE_NETWORK_PROJECT, "网络工程");
+    private static final MajorEnum INFO_SECURITY = new MajorEnum(CODE_INFO_SECURITY, "信息安全");
+    private static final MajorEnum IOT = new MajorEnum(CODE_IOT, "物联网工程");
+    private static final MajorEnum DIGITAL_META_PROJECT = new MajorEnum(CODE_DIGITAL_META_PROJECT, "数字媒体技术");
 
     private String code;
     private String name;
@@ -28,12 +26,11 @@ public class MajorEnum {
             return null;
         }
         switch (code) {
-            case CODE_TONGXIN : return TONGXIN;
-            case CODE_COMPUTER_SCIENCE : return COMPUTER_SCIENCE;
             case CODE_SOFT_PROJECT : return SOFT_PROJECT;
-            case CODE_IOT : return IOT;
             case CODE_NETWORK_PROJECT : return NETWORK_PROJECT;
-            case CODE_DIGITAL_META : return DIGITAL_META;
+            case CODE_INFO_SECURITY : return INFO_SECURITY;
+            case CODE_IOT : return IOT;
+            case CODE_DIGITAL_META_PROJECT : return DIGITAL_META_PROJECT;
             default:throw new IllegalArgumentException("请核对传入参数");
         }
     }
